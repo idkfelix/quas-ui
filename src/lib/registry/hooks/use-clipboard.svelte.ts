b6c3ba3664328a3ae5f2d627c7f8e2079c1a03d9
Felix Coulthard <felix.coulthard@gmail.com>
2026-08-02 19:39:37 +1000

    use-clipboard

---
+++ src/lib/registry/hooks/use-clipboard.svelte.ts
@@ -0,0 +1,37 @@
+export class UseClipboard {
+	#delay: number;
+	#copiedStatus = $state<boolean>();
+	#timeout: ReturnType<typeof setTimeout> | undefined = undefined;
+
+	constructor(delay: number = 500) {
+		this.#delay = delay;
+	}
+
+	async copy(text: string) {
+		if (this.#timeout) {
+			this.#copiedStatus = undefined;
+			clearTimeout(this.#timeout);
+		}
+
+		this.#copiedStatus = await copyText(text);
+
+		this.#timeout = setTimeout(() => {
+			this.#copiedStatus = undefined;
+		}, this.#delay);
+
+		return this.#copiedStatus;
+	}
+
+	get copied() {
+		return this.#copiedStatus;
+	}
+}
+
+export async function copyText(text: string): Promise<boolean> {
+	try {
+    await navigator.clipboard.writeText(text);
+    return true;
+	} catch {
+		return false;
+	}
+}
