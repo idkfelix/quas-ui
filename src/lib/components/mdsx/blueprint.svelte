51893b7fa0aa11f5ac4422f5985f86facfcdb11c
Felix Coulthard <felix.coulthard@gmail.com>
2026-08-08 22:41:33 +1000

    update link component & remove redundant a tag component

--- src/lib/components/mdsx/blueprint.svelte
+++ src/lib/components/mdsx/blueprint.svelte
@@ -1,15 +1,15 @@
 <script lang="ts" context="module">
+	export { default as p } from "./p.svelte";
 	export { default as h1 } from "./h1.svelte";
 	export { default as h2 } from "./h2.svelte";
 	export { default as h3 } from "./h3.svelte";
 	export { default as h4 } from "./h4.svelte";
-	export { default as p } from "./p.svelte";
-	export { default as a } from "./a.svelte";
 	export { default as li } from "./li.svelte";
 	export { default as ol } from "./ol.svelte";
 	export { default as ul } from "./ul.svelte";
 	export { default as hr } from "./hr.svelte";
 	export { default as strong } from "./strong.svelte";
+	export { Root as a } from "$lib/registry/ui/link/index.js";
 </script>
 
 <script lang="ts">
--- src/lib/registry/ui/link/link.svelte
+++ src/lib/registry/ui/link/link.svelte
@@ -2,9 +2,18 @@
 	import { cn } from "$lib/utils.js";
 	import type { HTMLAnchorAttributes } from "svelte/elements";
 
-	let { children, class: className, ...rest }: HTMLAnchorAttributes = $props();
+	let { children, class: className, href, ...rest }: HTMLAnchorAttributes = $props();
+	const internal = $derived(href?.startsWith("/") || href?.startsWith("#"));
+	const rel = $derived(!internal ? "noopener noreferrer" : undefined);
+	const target = $derived(!internal ? "_blank" : undefined);
 </script>
 
-<a {...rest} class={cn("font-medium text-foreground underline underline-offset-4", className)}>
+<a
+	{href}
+	{target}
+	{rel}
+	{...rest}
+	class={cn("font-medium text-foreground underline underline-offset-4", className)}
+>
 	{@render children?.()}
 </a>
