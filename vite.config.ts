ebf4a22705c0f5ea4bf40c572e3aeb4b3b412470
Felix Coulthard <felix.coulthard@gmail.com>
2026-08-09 14:27:21 +1000

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	assetsInclude: ["**/*.md"],
 	server: { forwardConsole: true },
+	ssr: { noExternal: Object.keys(pkg.devDependencies) },
 });
