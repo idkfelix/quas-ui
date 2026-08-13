dea2d66dc258191450637b94400b80f066bccc24
felix <felix.coulthard@gmail.com>
2026-08-13 15:32:48 +1000

    basic working

--- src/lib/components/api-table.svelte
+++ src/lib/components/api-table.svelte
@@ -1,26 +1,48 @@
 <script lang="ts" module>
-	export type PropReference = {
-		name: string;
-		type: string;
-		required?: boolean;
-		bindable?: boolean;
-		default?: string;
-		tooltip?: string;
-	};
-
-	export type ComponentReference = {
+	export type ComponentRef = {
 		name: string;
 		description: string;
-		props: PropReference;
+		props: Record<string, PropRef>;
+	};
+
+	export type PropRef = {
+		type: string;
+		tooltip?: string | string[];
+		required?: boolean;
+		bindable?: boolean;
+		defaultVal?: string;
 	};
 </script>
 
 <script lang="ts">
-	let { name, description, props }: ComponentReference = $props();
+	let { name, description, props }: ComponentRef = $props();
 </script>
 
-<div>
-	<h2>{name}</h2>
-	<p>{description}</p>
-	{JSON.stringify(props)};
-</div>
+<h2 class="mt-12 text-xl font-medium tracking-tight">{name}</h2>
+<p class="leading-relaxed">{description}</p>
+<table class="w-full">
+	<thead>
+		<tr>
+			<th>Prop</th>
+			<th>Type</th>
+			<th>Default</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each Object.entries(props) as [prop, ref] (prop)}
+			<tr>
+				<td class="flex gap-2 *:not-first:text-muted-foreground">
+					<span>{prop}</span>
+					{#if ref.required}
+						<span>[required]</span>
+					{/if}
+					{#if ref.bindable}
+						<span>[bindable]</span>
+					{/if}
+				</td>
+				<td>{ref.type}</td>
+				<td>{ref.defaultVal || "-"}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
