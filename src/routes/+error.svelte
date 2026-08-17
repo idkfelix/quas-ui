d5ddd1dc004574a8a8980231e604c1b10e5cfb43
Felix Coulthard <felix.coulthard@gmail.com>
2026-08-17 15:19:57 +1000

    fix metadata edgecase

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,11 @@
 	import { page } from "$app/state";
 </script>
 
+<svelte:head>
+	<title>{page.status} - quas ui</title>
+	<meta name="description" content={page.error?.message} />
+</svelte:head>
+
 <div class="flex flex-col items-center">
 	<h1 class="mt-24 text-4xl font-semibold">{page.status}</h1>
 	<p class="text-muted-foreground">{page.error?.message}</p>
