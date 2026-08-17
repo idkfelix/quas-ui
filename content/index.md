93485e2dd1542c64a058330657eb36cd85f735fe
Felix Coulthard <felix.coulthard@gmail.com>
2026-08-17 15:51:16 +1000

    rework sidebar items

--- content/index.md
+++ content/index.md
@@ -3,4 +3,4 @@ title: quas ui
 description: Some opinionated user interface components.
 ---
 
-[https://github.com/idkfelix/quas-ui](https://github.com/idkfelix/quas-ui)
+## Introduction
--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,28 +1,54 @@
-<script lang="ts">
+<script lang="ts" module>
 	import { docs } from "$content/index.js";
-	import * as Sidebar from "$lib/registry/ui/sidebar/index.js";
 
-	const groups = [
-		{ title: "Overview", docs: docs.filter((doc) => doc.folder === "root") },
-		{ title: "Components", docs: docs.filter((doc) => doc.folder === "components") },
-		{ title: "Hooks", docs: docs.filter((doc) => doc.folder === "hooks") },
+	type SidebarGroup = {
+		title: string;
+		items: {
+			title: string;
+			href: string;
+		}[];
+	};
+
+	const groups: SidebarGroup[] = [
+		{
+			title: "Overview",
+			items: [
+				{ title: "Home", href: "/" },
+				{ title: "Github", href: "https://github.com/idkfelix/quas-ui" },
+			],
+		},
+		{
+			title: "Components",
+			items: docs
+				.filter((doc) => doc.folder === "components")
+				.map(({ title, path }) => ({ title, href: `/${path}` })),
+		},
+		{
+			title: "Hooks",
+			items: docs
+				.filter((doc) => doc.folder === "hooks")
+				.map(({ title, path }) => ({ title, href: `/${path}` })),
+		},
 	];
+</script>
+
+<script lang="ts">
+	import * as Sidebar from "$lib/registry/ui/sidebar/index.js";
 </script>
 
 <Sidebar.Root class="sticky top-0 ml-auto hidden bg-transparent py-8 md:flex" collapsible="none">
 	<Sidebar.Content>
-		{#each groups as { title, docs } (title)}
+		{#each groups as { title, items } (title)}
 			<Sidebar.Group>
 				<Sidebar.GroupLabel class="font-medium text-muted-foreground">
 					{title}
 				</Sidebar.GroupLabel>
 				<Sidebar.GroupContent>
 					<Sidebar.MenuSub class="gap-1">
-						{#each docs as { title, path } (path)}
-							{@const href = `/${path.replace("index", "")}`}
+						{#each items as { title, href } (title)}
 							<Sidebar.MenuSubButton class="w-fit" size="sm">
 								{#snippet child({ props })}
-									<a {href} {...props}>{title}</a>
+									<a {href} target={href.at(0) !== "/" ? "_blank" : ""} {...props}>{title}</a>
 								{/snippet}
 							</Sidebar.MenuSubButton>
 						{/each}
